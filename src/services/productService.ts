e0485e20ec6ca9e8e1a3aa3d631ff1b5dbadbb94
Sujata-1231 <sujata2672singh2@gmail.com>
2025-01-06 16:18:14 +0530

    fix module error

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -1,6 +1,17 @@
 import axios from "axios";
 
 export const fetchProducts = async () => {
+  // for testing purpose only mention dummyjson not working as of now.
+  // const useMockData = true;
+  // if (useMockData) {
+  //   return mockProducts;
+  // }
   const response = await axios.get("https://dummyjson.com/products");
   return response.data.products;
 };
+
+// const mockProducts = [
+//   { id: 1, name: "Product 1", price: 100 },
+//   { id: 2, name: "Product 2", price: 200 },
+//   { id: 3, name: "Product 3", price: 300 },
+// ];
